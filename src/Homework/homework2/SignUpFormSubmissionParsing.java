e738b4a2bdfc828a95f20d78df25f60a2f67a312
hfathalla <56330598+hfathalla@users.noreply.github.com>
2019-12-28 01:05:59 -0500

    homework2day9

--- src/Homework/homework2/SignUpFormSubmissionParsing.java
+++ src/Homework/homework2/SignUpFormSubmissionParsing.java
@@ -1,5 +1,8 @@
 package Homework.homework2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SignUpFormSubmissionParsing {
     /**
      * open SignUpForm.html
@@ -10,4 +13,10 @@ public class SignUpFormSubmissionParsing {
      * hint: https://examples.javacodegeeks.com/core-java/net/url/parse-url-example/
      * like nameOfTheInput = valueOfTheInput
      */
+    public static void main(String[] args) {
+
+
+     System.setProperty("webdriver.chrome.driver", "/Users/mohannaelhetawy/Desktop/Chromedriver/chromedriver");
+    WebDriver driver = new ChromeDriver();
+        driver.get("http://fiddle.jshell.net/daulet2030/2qfux1r7/show/");
 }
